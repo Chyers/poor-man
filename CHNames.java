ceefba867d29304358bd4395d920e719626876ad
Cody H. <63369033+Chyers@users.noreply.github.com>
2025-08-31 16:36:21 -0400

    Added random java file

---
+++ CHNames.java
@@ -0,0 +1,23 @@
+//**************************************************************
+// Names.java Author: Cody Hyers
+//
+// Programming Assignment Chapter 2 Names List
+// Due 9/10/23
+// Write a program prints a list of student names
+// with their hometowns and intended major
+// ************************************************************
+public class CHNames
+{
+   // -------------------------- 
+   // main prints the list
+   // --------------------------
+   public static void main (String[] args)
+   {
+      System.out.println ();
+      System.out.println ("\tName\t\t\tHometown\t\t\tMajor") ;
+      System.out.println ("\t=====\t\t\t========\t\t\t========");
+      System.out.println ("\tCody\t\t\tDouglas\t\t\tComp Sci");
+      System.out.println ("\tDylan\t\t\tAlma\t\t\t\tMath");
+      System.out.println ("\tBrandon\t\tAlma\t\t\t\tComp Sci");
+    }
+}
